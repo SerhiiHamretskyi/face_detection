073b926529d6db6f5f6ee7aa8ca604298c08ed7e
Serhii <androsow10@gmail.com>
2024-11-28 22:09:47 +0100

    Fixed bug

--- structure/voice_processor.py
+++ structure/voice_processor.py
@@ -4,16 +4,17 @@ import pyttsx3
 
 class VoiceProcessor:
     def __init__(self):
+        self.tts_engine = pyttsx3.init()  # Initialize text-to-speech engine
+        self.tts_engine.setProperty('rate', 160)  # Set speech rate
         self.question_name = self.get_voice_input("Provide your name: ")
         self.output_dir = None
 
     def get_voice_input(self, prompt):
         recognizer = sr.Recognizer()
-        tts_engine = pyttsx3.init()
 
         # Use text-to-speech to give a prompt
-        tts_engine.say(prompt)
-        tts_engine.runAndWait()
+        self.tts_engine.say(prompt)
+        self.tts_engine.runAndWait()
 
         with sr.Microphone() as source:
             print(prompt)  # Display prompt text in the console as well
@@ -28,8 +29,8 @@ class VoiceProcessor:
 
         except sr.UnknownValueError:
             print("Sorry, I could not understand the audio.")
-            tts_engine.say("Sorry, I could not understand the audio.")
-            tts_engine.runAndWait()
+            self.tts_engine.say("Sorry, I could not understand the audio.")
+            self.tts_engine.runAndWait()
             return None
 
     def set_output_dir(self):
